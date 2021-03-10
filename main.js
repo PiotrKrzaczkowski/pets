25a7af4bb7fe831cc409557c9cf1bb3e47f9b37d
Piotr Krzaczkowski <slimshade93@wp.pl>
2021-03-10 23:44:36 +0100

    first global check

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 // MENU MOBILE
 const menuIcon = document.querySelector(".hamburger-menu");
 const mobileList = document.querySelector(".mobile-list ");
+const navMobile = document.querySelector(".nav-mobile");
 
 menuIcon.addEventListener("click", () => {
   menuIcon.classList.toggle("hamburger-menu--change");
   mobileList.classList.toggle("show");
+});
+
+window.addEventListener("scroll", (e) => {
+  if (window.scrollY >= 20) {
+    navMobile.classList.add("nav-mobile--bcg-col");
+  } else {
+    navMobile.classList.remove("nav-mobile--bcg-col");
+  }
 });
